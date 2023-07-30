e89a86bc6cd7f15c0c0f73bd0d5b68289af70f57
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-30 21:22:17 +0530

    Create internet problem grade.py

---
+++ internet problem grade.py
@@ -0,0 +1,15 @@
+m=int(input("how many marks want to enter:"))
+for i in range(0,m):
+  ma.append(m)
+print(ma)
+if 
+m=int(input("how many marks want to enter:"))
+for i in range(0,m):
+  ma.append(m)
+  for i in range(0,len(m)):
+    if m[i]>80:
+        g.apppend("Agrade")
+    elif m[i]<80:
+        g.append("fail")
+  
+print(ma)
