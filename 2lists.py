594c3b790744f2d8c3d926a9931277c943c3fdc3
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-30 21:00:25 +0530

    Create 2lists.py

---
+++ 2lists.py
@@ -0,0 +1,13 @@
+a=input("in which  u want to add=")
+t=[]
+s=[]
+if a=="t":
+  b=int(input("enter value to add="))
+  t.append(b)
+  print(t)
+elif a=="s":
+  c=int(input("enter value to add="))
+  s.append(c)
+  print(s)
+  
+  
