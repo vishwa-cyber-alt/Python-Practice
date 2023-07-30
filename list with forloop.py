e09445c1df2c35a399f41e984b9f43b750976bc4
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-30 21:02:14 +0530

    Create list with forloop.py

---
+++ list with forloop.py
@@ -0,0 +1,14 @@
+a=input("in which  u want to add=")
+t=[]
+s=[]
+if a=="t":
+    for i in range(0,6):
+        b=int(input("enter value to add="))
+        t.append(b)
+    print(t)
+elif a=="s":
+  c=int(input("enter value to add="))
+  s.append(c)
+  print(s)
+  
+  
