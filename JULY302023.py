7bd3ff9fb1b0448c1bfccaca9ec6844f1d8551f2
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-30 21:09:53 +0530

    Create JULY302023.py

---
+++ JULY302023.py
@@ -0,0 +1,17 @@
+a=input("in which  u want to add=")
+t=[]
+s=[]
+if a=="t":
+    ht=input("how many data you want to add ")
+    for i in range(0,h):
+        b=int(input("enter value to add="))
+        t.append(b)
+    print(t)
+elif a=="s":
+    hs=input("how many data you want to add ")
+    for i in range(0,hs):
+        c=int(input("enter value to add="))
+        s.append(c)
+    print(s)
+  
+  
