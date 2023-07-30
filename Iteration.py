9305e0e4a4078c0ad220aee73d16b222532975cf
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-30 12:23:36 +0530

    Create Iteration.py

---
+++ Iteration.py
@@ -0,0 +1,10 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+t=[]
+s=[]
+t.append(3)
+t.append(8)
+t.append(9)
+print(t)
+for i in range(0,len(t)):
+    print(t[i])
