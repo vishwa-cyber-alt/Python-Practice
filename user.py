8b9278607f69f4fe00a15ec302c05e1e50923e9c
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-31 16:05:46 +0530

    Create user.py

---
+++ user.py
@@ -0,0 +1,8 @@
+name=input("enter name=")
+age=int(input("enter age="))
+ed=input("enter Qualification=")
+print("entered details are ")
+print("name : ",name)
+print("age: ",age)
+print("ed: ",ed)
+#store array then pos
