8d099fca4a8a11c42f96a6c5840415f60555ecb6
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-31 15:09:56 +0530

    Update internet problem grade.py

--- internet problem grade.py
+++ internet problem grade.py
@@ -1,14 +1,17 @@
-m=int(input("how many marks want to enter:"))
-for i in range(0,m):
-  ma.append(m)
-print(ma)
-m=int(input("how many marks want to enter:"))
-for i in range(0,m):
-  ma.append(m)
-  for i in range(0,len(m)):
-    if m[i]>80:
-        g.apppend("Agrade")
-    elif m[i]<80:
-        g.append("fail")
-  
-print(ma)
+m = int(input("How many marks do you want to enter: "))
+ma = []
+
+for i in range(0, m):
+    mark = int(input("Enter the mark: "))
+    ma.append(mark)
+
+print("Entered marks:", ma)
+
+grades = []
+for mark in ma:
+    if mark >= 80:
+        grades.append("A grade")
+    else:
+        grades.append("Fail")
+
+print("Grades:", grades)
