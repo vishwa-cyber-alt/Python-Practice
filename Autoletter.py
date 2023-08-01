619b3b1649721294f7324c020106fb97c7839004
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-01 13:05:33 +0530

    Create Autoletter.py

---
+++ Autoletter.py
@@ -0,0 +1,4 @@
+a=input("hiring manager name:")
+b=input("job title:")
+c=input("company name:")
+print("Dear ",a," I am writing to express my interest in the amazing opportunity for the ",b,"position at ",c,". I believe that my skills and experience make me a strong candidate for this position within your organization.")
