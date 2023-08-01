d353dd4bf267c19fdae10a6291bc4f6a579bb973
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-01 13:58:23 +0530

    Create Time.py

---
+++ Time.py
@@ -0,0 +1,12 @@
+t=int(input("time:"))
+a=int(input("command:"))
+l=["youtube","insta","facebook"]
+if a==1 and t==4:
+   print("opening youtube")
+elif a==1 and t!=4:
+    print ("can't youtube")
+elif a==2 and t==6:
+   print("opening insta")
+elif a==3 and t==8:
+   print("opening facebook")
+   
