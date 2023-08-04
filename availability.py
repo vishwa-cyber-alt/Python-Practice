6816ec949a099afeecd7b0f94f848f629ca90d1d
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-04 13:39:46 +0530

    Create availability.py

---
+++ availability.py
@@ -0,0 +1,7 @@
+seat_booked=[1,3,5,7,9,11]
+seat_notbooked=[2,4,6,8,10]
+availability=int(input("enter to check availability="))
+if availability in seat_booked:
+    print("booked")
+elif availability in seat_notbooked:
+    print("notbooked")
