6f4190cf3c9500db3e5b2f52fa76ea9235e72e03
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-04 19:09:04 +0530

    Create table.py

---
+++ table.py
@@ -0,0 +1,9 @@
+name="Vishwa V"
+Qualification="BTECH"
+dob=14032002
+sname="Ravi"
+sQualification="MTECH"
+sdob=23092023
+print("Name\t\tQualification\t\tDOB")
+print(name,"\t",Qualification,"\t\t\t",dob)
+print(sname,"\t\t",sQualification,"\t\t\t",sdob)
