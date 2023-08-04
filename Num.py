eea9f0fb879f1b818d388d53bf1d6c252d605242
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-04 23:18:00 +0530

    Create Num.py

---
+++ Num.py
@@ -0,0 +1,9 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+a=[3,2,4]
+n=2
+if n*n in a:
+    print("yes")
+else:
+    print("no"
