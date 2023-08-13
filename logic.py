6c3922d7580764791e9c58499b3ab6ab90ffbff2
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-13 21:02:22 +0530

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,7 @@
+a=input("for which platform you want to build apps:")
+if a=="android":
+    print("use java android studio")
+elif a=="ios":
+    print("use swift")
+elif a=="both":
+    print("use flutter or react native")
