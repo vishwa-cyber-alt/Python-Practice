f13da688a6d6261050a0d4b6d2d6705da130c800
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-19 20:36:43 +0530

    Create question and answer.py

---
+++ question and answer.py
@@ -0,0 +1,15 @@
+count = 0
+
+a = int(input())
+q1 = input("Capital of India: 1) Delhi 2) Bihar: ")
+if q1 == "1":
+    count += 1
+
+q2 = input("HTML stands for: 1) Hyper 2) Hyper Text markup language: ")
+if q2 == "2":
+    count += 1
+q3 = input("IT : 1) information technology 2) information: ")
+if q3 == "1":
+    count += 1
+print("Number of correct answers:", count)
+print("created by Vishwa V")
