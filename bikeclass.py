6c4c253c7beabf9ddc7d6f34e75416843fb18cc7
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-25 13:33:49 +0530

    Create bikeclass.py

---
+++ bikeclass.py
@@ -0,0 +1,23 @@
+class Bike:
+    name=""
+    price=0
+bike=Bike()
+bike.name="honda"
+bike.price=43000
+bike.model=2023
+print(bike.name,bike.price,bike.model)
+a="bike"
+print(a,"name=",bike.name)
+print(a,"price=",bike.price)
+print(a,"model=",bike.model)
+
+
+
+
+Output:
+----------
+
+honda 43000 2023
+bike name= honda
+bike price= 43000
+bike model= 2023
