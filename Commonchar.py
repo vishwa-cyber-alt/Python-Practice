28c6f02d6001f122d026bf00bab6e74f8d9e29de
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-28 09:28:52 +0530

    Create Commonchar.py

---
+++ Commonchar.py
@@ -0,0 +1,8 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+a="vishwa"
+b="btechcse"
+z=set(a)
+y=set(b)
+print(z&y)
