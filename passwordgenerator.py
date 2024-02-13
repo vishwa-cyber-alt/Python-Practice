ec34a0cff500f0234477d5ec8db6ea752a1b1a81
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2024-02-13 21:27:35 +0530

    Create passwordgenerator.py

---
+++ passwordgenerator.py
@@ -0,0 +1,25 @@
+import random
+passwords = [
+    "P@ssw0rd123",
+    "SecurePass789",
+    "StrongP@55",
+    "Secret123!",
+    "MyPassword456",
+    "SuperSecurePwd",
+    "AmazingPass123",
+    "UltraSafePass",
+    "Password1234",
+    "TopSecretPwd!",
+    "SafeAndSound1",
+    "NewPassword567",
+    "HiddenPwd@2024",
+    "SecureAccess123",
+    "ProtectedPass!",
+    "StrongSecurePwd",
+    "SecretKey1234",
+    "MasterPass789",
+    "SafeAndSecure1",
+    "UniquePass!234"
+]
+a=random.choices(passwords)
+print(a)
