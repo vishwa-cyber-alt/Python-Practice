b072bc0fda7a0e75ecf79cf6801784218ab00bad
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2025-03-18 21:18:08 +0530

    Create method overriding.py

---
+++ method overriding.py
@@ -0,0 +1,10 @@
+class ShareMarket:
+    def SharePrice(self):
+        print("$300")
+class Latest:
+    def SharePrice(self):
+        print("$800")   
+vishwa=ShareMarket()
+john=Latest()
+vishwa.SharePrice()
+john.SharePrice()
